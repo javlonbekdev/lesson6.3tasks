a1efb17ae1419f706afde34bf1a1538aa771da62
Javlonbek <javlonbekalhamdulillah@gmail.com>
2022-01-30 15:20:02 +0500

    Initial Commit

---
+++ lesson6.3tasks/lesson6_3tasksApp.swift
@@ -0,0 +1,17 @@
+//
+//  lesson6_3tasksApp.swift
+//  lesson6.3tasks
+//
+//  Created by Javlonbek on 30/01/22.
+//
+
+import SwiftUI
+
+@main
+struct lesson6_3tasksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
